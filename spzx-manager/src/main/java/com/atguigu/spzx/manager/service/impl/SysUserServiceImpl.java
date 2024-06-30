a27364f823f1ee590c86a74ac577a0da5cf1faea
Henry <15941356654@163.com>
2024-06-30 15:57:23 +0800

    登录接口+统一异常处理

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,58 @@
+package com.atguigu.spzx.manager.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.atguigu.spzx.common.exception.GuiguException;
+import com.atguigu.spzx.manager.mapper.SysUserMapper;
+import com.atguigu.spzx.manager.service.SysUserService;
+import com.atguigu.spzx.model.dto.system.LoginDto;
+import com.atguigu.spzx.model.entity.system.SysUser;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.atguigu.spzx.model.vo.system.LoginVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class SysUserServiceImpl implements SysUserService {
+
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public LoginVo login(LoginDto loginDto) {
+
+        // 根据用户名查询用户
+        SysUser sysUser = sysUserMapper.selectByUserName(loginDto.getUserName());
+        if(sysUser == null) {
+            throw new GuiguException(ResultCodeEnum.LOGIN_ERROR);
+        }
+
+        // 验证密码是否正确
+        String inputPassword = loginDto.getPassword();
+        String md5InputPassword = DigestUtils.md5DigestAsHex(inputPassword.getBytes());
+        if(!md5InputPassword.equals(sysUser.getPassword())) {
+            throw new GuiguException(ResultCodeEnum.LOGIN_ERROR);
+        }
+
+        // 生成令牌，保存数据到Redis中
+        String token = UUID.randomUUID().toString().replace("-", "");
+        redisTemplate.opsForValue().set("user:login:" + token , JSON.toJSONString(sysUser) , 30 , TimeUnit.MINUTES);
+
+        // 构建响应结果对象
+        LoginVo loginVo = new LoginVo() ;
+        loginVo.setToken(token);
+        loginVo.setRefresh_token("");
+
+        // 返回
+        return loginVo;
+
+
+    }
+}
